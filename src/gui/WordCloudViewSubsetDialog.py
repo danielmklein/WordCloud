f58e3ccaf025fbe2301d96c0f370636b7adf6222
dmklein <daniel.klein.dmk@gmail.com>
2014-04-26 20:47:59 -0500

    Added dialog box to display detailed info for a given opinion in a subset.

--- src/gui/WordCloudViewSubsetDialog.py
+++ src/gui/WordCloudViewSubsetDialog.py
@@ -24,11 +24,10 @@ class WordCloudViewSubsetDialog(wx.Dialog):
         self.name_label = wx.StaticText(self.panel, -1, 
                                    "Subset Name:  {0}".format(self.subset_name))
         
-        subset = self.wc_core.subsets[self.subset_name]
-        titles = [doc.doc_metadata.case_title for doc in subset]
+        self.subset = self.wc_core.subsets[self.subset_name]
+        self.titles = [doc.doc_metadata.case_title for doc in self.subset]
         self.doc_list = wx.ListBox(self.panel, size=(500, 500), 
-                                      choices=titles)
-        
+                                      choices=self.titles)
         
         self.view_details = wx.Button(self.panel, wx.ID_CLOSE, "View Document Details")
         self.view_details.Bind(wx.EVT_BUTTON, self.OnViewDetails)
@@ -44,26 +43,61 @@ class WordCloudViewSubsetDialog(wx.Dialog):
         
         
     def OnViewDetails(self, event):
-        '''
-        TODO: populate the text for this dialog box. Maybe this shouldn't 
-        be a MessageDialog, but rather something else.
-        '''
-        info_string = "This is where I will plop all the document info."
-        wx.MessageDialog(None, info_string, "placeholder", wx.OK).ShowModal()
+        title = self.titles[self.doc_list.GetSelection()]
+        for item in self.subset:
+            if title == item.doc_metadata.case_title:
+                document = item
+                break
+        else:
+            raise Exception("Should have been able to find document in list, but couldn't.")
+        dia = WordCloudViewDocDetailsDialog(self, document, "View Details")
+        dia.ShowModal()
+        dia.Destroy()
         
-        
-    def build_info_string(self, subset):
+    
+    
+
+class WordCloudViewDocDetailsDialog(wx.Dialog):
+    '''
+    Daniel Klein
+    Computer-Based Honors Program
+    The University of Alabama
+    4.26.2014  
+    
+    This dialog box displays the details about a document in a subset.
+    '''
+    
+    def __init__(self, parent, document, title="View Details"):
+        wx.Dialog.__init__(self, parent, -1, title, size=(1000, 1000))
+        self.panel = wx.Panel(self, -1)
+        self.main_box = wx.BoxSizer(wx.VERTICAL)
         '''
-        TODO: This is terrible. Make it better. It's also really really slow.
-        This was taken from WordCloudFrame. I don't think I want to do it this way.
-        It's probably better to just create a new type of dialog box to display document info.
+        TODO: fit text to width (especially opinion_text)
         '''
-        info_string = "CASE TITLE                 OPINION AUTHOR  OPINION TYPE\n"
-        for opinion in subset:
-            info_string += opinion.doc_metadata.case_title + "\t"
-            info_string += opinion.doc_metadata.opinion_author + "\t"
-            info_string += opinion.doc_metadata.opinion_type +"\n"
-        return info_string
+        case_title = wx.StaticText(self.panel, -1, 
+                                   "Case Title:  {0}".format(document.doc_metadata.case_title))
+        case_cite = wx.StaticText(self.panel, -1,
+                                  "Case Citation: {0}".format(document.doc_metadata.case_full_cite))
+        case_dates = wx.StaticText(self.panel, -1,
+                                   "Case Dates: {0}".format(document.doc_metadata.case_dates))
+        case_disposition = wx.StaticText(self.panel, -1,
+                                         "Case Disposition: {0}".format(document.doc_metadata.case_disposition))
+        opinion_author = wx.StaticText(self.panel, -1,
+                                       "Opinion Author: {0}".format(document.doc_metadata.opinion_author))
+        opinion_text_label = wx.StaticText(self.panel, -1, "Opinion Text:")
+        opinion_text = wx.StaticText(self.panel, -1, document.doc_text)
         
-        
+        self.main_box.Add(case_title)
+        self.main_box.Add(case_cite)
+        self.main_box.Add(case_dates)
+        self.main_box.Add(case_disposition)
+        self.main_box.Add(opinion_author)
+        self.main_box.Add(opinion_text_label)
+        self.main_box.Add(opinion_text)
+        self.panel.SetSizer(self.main_box)
+        self.panel.Layout()
+        self.panel.Fit()
+        self.Fit()
+    
+    
         
