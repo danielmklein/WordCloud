82f5e4f6ef405d2e1eb7ccbc1276f5c6a6a9fe0a
dmklein <daniel.klein.dmk@gmail.com>
2014-09-09 08:46:32 -0500

    Added a couple DocumentStorage methodss and figured out how I'm going to do stemming and stop word removal.

--- src/core/javacore/DocumentStorage.java
+++ src/core/javacore/DocumentStorage.java
@@ -1,5 +1,6 @@
 package core.javacore;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,7 @@ import java.util.Map;
 *   to store the list of terms in the document and each of their 
 *   frequencies.
 */
-public class DocumentStorage
+public class DocumentStorage extends Document
 {
 
     /**
@@ -21,18 +22,42 @@ public class DocumentStorage
      */
     public DocumentStorage(Metadata docMetadata, String docText, String outputFilename)
     {
-
+        super(docMetadata, docText, outputFilename);
         // TODO Auto-generated constructor stub
     }
     
     private Map<String, Map<String, Double>> buildTermList(List<String> splitText)
     {
         
+        Map<String, Map<String, Double>> termList = new HashMap<String, Map<String, Double>>();
+        
+        for (String term : splitText)
+        {
+            if (!termList.containsKey(term))
+            {
+                Map<String, Double> metrics = new HashMap<String, Double>();
+                metrics.put("tf", null);
+                metrics.put("count", new Double(0));
+            } else
+            {
+                Double oldCount = termList.get(term).get("count");
+                termList.get(term).put("count", oldCount + 1);
+            }
+        }
+        
+        return termList;
     }
     
     private Map<String, Map<String, Double>> populateTermFreqs(Map<String, Map<String, Double>> termList)
     {
         
+        for (String term : termList.keySet())
+        {
+            Double tf = this.calcTermFreq(term);
+            termList.get(term).put("tf", tf);
+        }
+        
+        return termList;
     }
     
     private List<String> filterText(String text, boolean shouldDropPropNouns)
@@ -47,6 +72,7 @@ public class DocumentStorage
     
     private List<String> stemText(List<String> wordList)
     {
+        // TODO: use opennlp.tools.stemmer.Stemmer.
         
     }
     
